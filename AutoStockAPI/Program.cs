822951309ad6a92590607f39eee6a4e0b6942740
Erdem Mete <erdemmete7060@gmail.com>
2026-04-30 21:08:22 +0300

    v1 publish

--- AutoStockAPI/Program.cs
+++ AutoStockAPI/Program.cs
@@ -31,12 +31,15 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 
